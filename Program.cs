46d9096c27bee23a92c775c1cc37a1c37ab428b4
Nadya <nadya0707@yandex.ru>
2023-02-05 02:17:03 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void Input_array (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array [i] = new Random().Next(100,1000);
+}
+int release(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array [i] % 2 == 0)
+            count++;
+    return count;
+}
+Console.Write("Введите колличество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array =new int [n];
+Input_array(array);
+Console.WriteLine($"[{string.Join(" , " , array)}]");
+Console.WriteLine(release(array));
